ce5cb20efb5644562eb82eb0d3e7953794864d2b
Grant Heath <heathgr@gmail.com>
2020-01-26 10:08:34 -0700

    Adds a react hook that subscribes to a store.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,3 @@
-import { Component, ReactNode } from 'react';
-import { default as Store } from 's-is-for-store';
-export interface StoreProviderProps<T> {
-    store: Store<T>;
-    children: (state: T) => ReactNode;
-}
-export declare class StoreProvider<T> extends Component<StoreProviderProps<T>, T> {
-    constructor(props: any);
-    handleState: (state: any) => Promise<{}>;
-    render(): ReactNode;
-}
+import Store from 's-is-for-store';
+export declare const useStore: <T>(store: Store<T>) => T;
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import { Component, ReactNode } from 'react'
-import { default as Store } from 's-is-for-store'
+import { useEffect, useState} from 'react'
+import Store from 's-is-for-store'
 
-export interface StoreProviderProps<T> {
-  store: Store<T>
-  children: (state: T) => ReactNode
-}
+export const useStore = <T>(store: Store<T>) => {
+  const [state, setState] = useState(store.current)
 
-export class StoreProvider<T> extends Component<StoreProviderProps<T>, T> {
-  constructor(props: any) {
-    super(props)
+  useEffect(() => {
+    return store.subscribe((newState) => {
+      setState(newState)
+    })
+  }, [])
 
-    const store = this.props.store
-
-    this.state = store.getState()
-
-    store.subscribe(this.handleState)
-  }
-
-  public handleState = (state: any) => new Promise((resolve) => {
-    this.setState(state, () => resolve(state))
-  })
-
-  render() {
-    return this.props.children(this.state)
-  }
+  return state
 }
