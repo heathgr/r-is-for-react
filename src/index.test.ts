6bedba62f71b3fe83a6a3376a1c98b2901709cd5
Grant Heath <heathgr@gmail.com>
2020-07-15 22:14:26 -0600

    Improve test suite.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,21 @@
-import { createElement as e } from 'react'
+import { createElement as e, useState } from 'react'
 import { act } from 'react-dom/test-utils'
 import { mount } from 'enzyme'
 import { createStore } from 's-is-for-store'
 import { useStore } from '../src/index'
+
+const stateSpy = jest.fn()
+const setStateSpy = jest.fn()
+
+jest.mock('react', () => {
+  const actual = jest.requireActual('react')
+
+  return {
+    ...actual,
+    useState: () => [stateSpy, setStateSpy],
+    useEffect: jest.fn(),
+  }
+})
 
 describe('r-is-for-react', () => {
   it('Should create a React Hook for subscribing to a Store.', () => {
@@ -26,5 +39,18 @@ describe('r-is-for-react', () => {
     })
 
     expect(subject.text()).toEqual('test state 2')
+  })
+
+  it('Should call set state.', () => {
+    interface TestState {
+      message: string
+    }
+    const testStore = createStore<TestState>({
+      message: 'test state 1'
+    })
+
+    const subject = useStore(testStore)
+
+    expect(stateSpy).toHaveReturnedWith(true)
   })
 })
